c2704cdc0e5ad95d2aa979414ff3613763677fdf
Calem Bendell <calem.j.bendell@gmail.com>
2014-04-29 11:25:15 -0400

    added file for scrapedfile io, added extended metadata csv

--- simulator/simulator/lab/fumehood.py
+++ simulator/simulator/lab/fumehood.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class Fumehood:
     kind
     height
@@ -10,23 +13,31 @@ class Fumehood:
     maxcfm
     mincfm
     occupied
-    bac
-    tag
-    building
-    room
-    hoodnumber
-    department
-    uselevel
+    metadata
 
     def __init__(self, kind, height, width, minflowrate, maxflowrate, facevelocityoccupied, facevelocityunoccupied, maxsash, maxcfm, mincfm):
-	    self.kind = kind
-	    self.height = height
-	    self.width = width
-	    self.minflowrate = minflowrate
-	    self.maxflowrate = maxflowrate
-	    self.facevelocityoccupied = facevelocityoccupied
-	    self.facevelocityunoccupied = facevelocityunoccupied
-	    self.maxsash = maxsash
-	    self.maxcfm = maxcfm
-	    self.mincfm = mincfm
+      self.kind = kind
+      self.height = height
+      self.width = width
+      self.minflowrate = minflowrate
+      self.maxflowrate = maxflowrate
+      self.facevelocityoccupied = facevelocityoccupied
+      self.facevelocityunoccupied = facevelocityunoccupied
+      self.maxsash = maxsash
+      self.maxcfm = maxcfm
+      self.mincfm = mincfm
 
+    def facevelocity(self):
+      if self.occupied:
+        return self.facevelocityoccupied
+      else:
+        return self.facevelocityunoccupied
+
+    def faceintakecfm(self):
+      return self.facevelocity * self.maxsash * self.height * self.width / 144
+
+    def fumehoodcfm(self):
+      return np.min([self.maxcfm, np.max([self.mincfm, self.faceintakecfm])])
+
+def load_fumehood_metadata(f):
+  metadata = pd.read_csv(f)
