4d186f6cd870fdb9f3b6d27d5aeeb60969abfab2
Calem Bendell <calem.j.bendell@gmail.com>
2014-05-08 09:24:47 -0400

    remove unnecessary plotting functions

--- simulator_python/simulator/simulatorstats/cfm_values.py
+++ simulator_python/simulator/simulatorstats/cfm_values.py
@@ -62,36 +62,3 @@ def plot_summary_per_lab(results_by_lab, fig_title):
     plt.tight_layout()
     plt.savefig(fig_title + str(k) + '-allfumehoods.pdf')
 
-
-def plot_summary_per_lab_mean(results_by_lab, fig_title):
-
-  for k, v in results_by_lab:
-    data = pd.Series(v.sum(axis=1).ix[:])
-    fig = bootstrap_plot(data, size=200, samples=1000)
-    plt.tight_layout()
-    plt.savefig(fig_title + "-" + str(k) + "-sum-bootstrap.pdf")
-    data = pd.Series(v.mean(axis=1).ix[:])
-    fig = bootstrap_plot(data, size=200, samples=1000)
-    plt.tight_layout()
-    plt.savefig(fig_title + "-" + str(k) + "-mean-bootstrap.pdf")
-
-  fig = plt.figure()
-  results_by_lab.aggregate(np.mean).describe().plot(kind='bar')
-  plt.tight_layout()
-  plt.savefig(fig_title + "-" + str(k) + "-bardescription.pdf")
-
-def plot_diff_per_lab(a, b, fig_title):
-  fig = plt.figure()
-  ax = plt.subplot(111)
-  for k, v in a:
-    ax.plot(v.mean(axis=1), label=(str(k) + 'a-mean'))
-    ax.plot(v.std(axis=1), label=(str(k) + 'a-std'))
-  for k, v in b.iteritems():
-    ax.plot(v, label=(str(k) + 'b-sum'))
-  box = ax.get_position()
-  ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                   box.width, box.height * 0.9])
-  ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-            fancybox=True, shadow=True, ncol=2)
-  plt.tight_layout()
-  plt.savefig(fig_title + "-std-mean.pdf")
